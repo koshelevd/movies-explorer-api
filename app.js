a35b7847e1e7efbd90b516cc37ad4c3e2dd44c5d
Dmitrii Koshelev <me@koshelev.net>
2021-07-05 15:08:15 +0300

    Connect to db

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const {
+  DB_HOST = 'localhost',
+  DB_PORT = '27017',
+  DB_NAME = 'bitfilmsdb',
+  PORT = 3000,
+  API_PATH = '',
+} = process.env;
 
 const app = express();
+
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
