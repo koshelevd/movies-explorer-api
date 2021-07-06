62e54384dd7d962e756da9d7ff000c2cb61ad3bc
Dmitrii Koshelev <me@koshelev.net>
2021-07-06 13:32:21 +0300

    Remove console error logging

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ app.use(errors());
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  console.log(err);
   res.status(statusCode).send({
     message: statusCode === 500 ? 'Internal server error' : message,
   });
