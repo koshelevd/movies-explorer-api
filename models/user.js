250909b7f3546a84e90f5dd2e3cd25b45504f731
Dmitrii Koshelev <me@koshelev.net>
2021-07-06 09:31:06 +0300

    Add findUserByCredentials to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const { isEmail } = require('validator');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const validateEmail = [
   {
@@ -35,5 +39,25 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+const authFailed = new UnauthorizedError('Email or password are incorrect');
+
+function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then(user => {
+      if (!user) {
+        return Promise.reject(authFailed);
+      }
+      return bcrypt.compare(password, user.password).then(matched => {
+        if (!matched) {
+          return Promise.reject(authFailed);
+        }
+        return user;
+      });
+    });
+}
+
+userSchema.statics.findUserByCredentials = findUserByCredentials;
 
 module.exports = mongoose.model('user', userSchema);
