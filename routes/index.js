a8750a660840a526e53aa7180eec7ad50fea18ae
Dmitrii Koshelev <me@koshelev.net>
2021-07-07 11:08:40 +0300

    Refactor to use constant messages and use auth before default routes

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
 
-const { API_PATH = '' } = process.env;
 const NotFoundError = require('../errors/NotFoundError');
 const auth = require('../middlewares/auth');
+const messages = require('../utils/constants');
 
 const authRoutes = require('./auth');
 const usersRoutes = require('./users');
 const moviesRoutes = require('./movies');
 
-router.use(`${API_PATH}/`, authRoutes);
-router.use(`${API_PATH}/users`, auth, usersRoutes);
-router.use(`${API_PATH}/movies`, auth, moviesRoutes);
+router.use('/', authRoutes);
+router.use(auth);
+router.use('/users', usersRoutes);
+router.use('/movies', moviesRoutes);
 
 router.use((req, res, next) => {
-  next(new NotFoundError('Endpoint or method not found'));
+  next(new NotFoundError(messages.pathNotFound));
 });
 
 module.exports = router;
