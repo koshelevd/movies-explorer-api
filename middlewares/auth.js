0f833c4b6b96bd57ac4b87a6138cd22ed6794704
Dmitrii Koshelev <me@koshelev.net>
2021-07-07 09:20:35 +0300

    Remove eslint-disable-next-line and fix code formatting

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 const { NODE_ENV, JWT_SECRET } = process.env;
 const unauthorizedError = new UnauthorizedError();
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
